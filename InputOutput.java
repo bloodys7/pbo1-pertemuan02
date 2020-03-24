a016999b5d804000c6fec1271c328ca17fb6494d
bloodys7 <DHaris454@mail.com>
2020-03-24 16:29:50 +0800

    Update InputOutput.java

--- InputOutput.java
+++ InputOutput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class InputOut{
+public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
